e25b50ef69215ac6559ef27a1616a857d4787313
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-17 15:28:50 +0200

    Fixed the trigger event onChange by giving it props

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,11 @@ const ImageLinkForm = ({ onInputChange }) => {
       {/* Used some tachyons to add some styling */}
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" onChange />
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
             Detect
           </button>
