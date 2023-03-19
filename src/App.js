af937de96cb13edcb43a9a06cd8fd5d1c79e5bdc
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-19 23:54:30 +0200

    Added component - FaceRecognition component to display image in our website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -86,7 +87,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*<FaceRecognition />*/}
+        <FaceRecognition />
       </div>
     );
   }
