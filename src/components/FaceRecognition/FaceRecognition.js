c79c128ceb47f27e0345c524469454614e15e976
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-20 00:11:44 +0200

    Added the image to the page to be loaded when submitted

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const FaceRecognition = () => {
   return (
-    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      {/*Using tachyons to style our Sign Out paragraph*/}
-      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
-    </nav>
+    <div className="center">
+      <img src={"http://localhost:3000/image"}></img>
+    </div>
   );
 };
 
