e57a1b30b4df7854eac027746b27bb651288e4fd
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-21 20:11:07 +0200

    Add more functionality to the page Sign in and register

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,39 @@
 import React from "react";
 
-const Navigation = ({ onRouteChange }) => {
-  return (
-    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      {/*Using tachyons to style our Sign Out paragraph*/}
-      <p
-        // Function to make the Sign Out button take us back to the Sign In
-        onClick={() => onRouteChange("signin")}
-        className="f3 link dim black underline pa3 pointer"
-      >
-        Sign Out
-      </p>
-    </nav>
-  );
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        {/*Using tachyons to style our Sign Out paragraph*/}
+        <p
+          // Function to make the Sign Out button take us back to the Sign In
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        {/*Using tachyons to style our Sign Out paragraph*/}
+        <p
+          // Function to make the Sign Out button take us to Sign In form
+          onClick={() => onRouteChange("home")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRouteChange("home")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
 };
 
 export default Navigation;
