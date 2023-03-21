6b711dc48b54efd9699fd86c229cb38af9f789a4
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-21 17:34:44 +0200

    Added an event handler to the submit button

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SignIn = () => {
+const SignIn = ({ onRouteChange }) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
@@ -27,6 +27,7 @@ const SignIn = () => {
                 type="password"
                 name="password"
                 id="password"
+                onClick={onRouteChange}
               />
             </div>
           </fieldset>
