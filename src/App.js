14b7ed436e3db654dc61bcdd3ecc03ba6e296c08
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-21 17:42:19 +0200

    Modify route so that it accept the state we give it/ we're in

--- src/App.js
+++ src/App.js
@@ -101,8 +101,8 @@ class App extends Component {
       .catch((err) => console.log(err));
   };
 
-  onRouteChange = () => {
-    this.setState({ route: "home" });
+  onRouteChange = (route) => {
+    this.setState({ route: route });
   };
 
   render() {
