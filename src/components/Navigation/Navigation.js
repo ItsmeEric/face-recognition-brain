86dc39dfd60f8846279573dbeba734bd67e955f1
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-03-21 19:22:31 +0200

    Create Register component to sign up

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,6 +5,7 @@ const Navigation = ({ onRouteChange }) => {
     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
       {/*Using tachyons to style our Sign Out paragraph*/}
       <p
+        // Function to make the Sign Out button take us back to the Sign In
         onClick={() => onRouteChange("signin")}
         className="f3 link dim black underline pa3 pointer"
       >
