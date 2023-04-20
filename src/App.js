e3fb66eea939ca8a2be5f09c6579e0d23af16417
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-04-20 19:29:00 +0200

    Fix catch errors issue

--- src/App.js
+++ src/App.js
@@ -148,7 +148,8 @@ class App extends Component {
             .then((response) => response.json())
             .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(console.log);
         } else {
           window.alert("Not a valid Image URL.");
         }
